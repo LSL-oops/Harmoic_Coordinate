e793ee3944756647b839da9dac36b305b42cab73
LSL-oops <472980023@qq.com>
2019-07-18 17:18:40 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/HarmonicCoordinateDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HarmonicCoordinateDemo.dir/main.cpp.o"
+  "HarmonicCoordinateDemo.pdb"
+  "HarmonicCoordinateDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HarmonicCoordinateDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HarmonicCoordinateDemo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HarmonicCoordinateDemo.
+# This may be replaced when dependencies are built.
